a06a9524af94cac9ac212f6d7ec785d26cc954d6
nettniko <stevenwindka@gmail.com>
2024-08-05 22:17:11 -0400

    Delete js/routes2.js

--- js/routes2.js
+++
@@ -1,9 +0,0 @@
-import List from './pages/List2.js';
-import Leaderboard from './pages/Leaderboard2.js';
-import Roulette from './pages/Roulette2.js';
-
-export default [
-    { path: '/', component: List2 },
-    { path: '/leaderboard2', component: Leaderboard2 },
-    { path: '/roulette2', component: Roulette2 },
-];
