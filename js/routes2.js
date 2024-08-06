f24c4eb4f943458a2dc6967c9b3eba02fa08c27a
nettniko <stevenwindka@gmail.com>
2024-08-05 22:08:17 -0400

    Update routes2.js

--- js/routes2.js
+++ js/routes2.js
@@ -3,7 +3,7 @@ import Leaderboard from './pages/Leaderboard2.js';
 import Roulette from './pages/Roulette2.js';
 
 export default [
-    { path: '/', component: List },
-    { path: '/leaderboard', component: Leaderboard },
-    { path: '/roulette', component: Roulette },
+    { path: '/', component: List2 },
+    { path: '/leaderboard', component: Leaderboard2 },
+    { path: '/roulette', component: Roulette2 },
 ];
